809efe1ce73d46c542360ea3c1a59f6d92cd629f
baji-web <camrynchelsea01@gmail.com>
2024-02-08 13:34:34 +0800

    update

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,5 +1,11 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { staggerContainer, fadeIn, planetVariants } from '@/utils/motion';
+import { NewFeatures, TitleText, TypingText } from '@/components';
+import { newFeatures } from '@/constants';
+import styles from '@/app/style';
+
 const World = () => (
   <section>
     World section
